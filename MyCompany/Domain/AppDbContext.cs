7001ed567351f3bd30aecbc2dbe38dce3ecdf9ec
DESKTOP-98NGS9V\Александр <oshino.shinobu@yandex.ru>
2023-02-03 18:52:19 +0300

    Corrected the connectionstring, made changes to the AppDbContext for the convenience of deploying the site on other local machines

--- MyCompany/Domain/AppDbContext.cs
+++ MyCompany/Domain/AppDbContext.cs
@@ -8,7 +8,9 @@ namespace MyCompany.Domain
 {
     public class AppDbContext : IdentityDbContext<IdentityUser>
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+            Database.EnsureCreated();
+        }
 
         public DbSet<TextField> TextFields { get; set; } 
         public DbSet<ServiceItem> ServiceItems { get; set; }
--- MyCompany/Program.cs
+++ MyCompany/Program.cs
@@ -61,8 +61,9 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+   
 }
+app.UseHsts();
 //Подключаю систему маршрутизации
 app.UseRouting();
 app.UseStaticFiles();
